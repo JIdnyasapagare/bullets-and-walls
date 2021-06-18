b63a4173810507b1d9759781f803b9ec69004a9e
JIdnyasapagare <84508221+JIdnyasapagare@users.noreply.github.com>
2021-06-18 18:54:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   weight = random(30,52);
 thickness= random(22,83);
 
-bullet = createSprite(50,200,50,50);
+bullet = createSprite(50,200,50,15);
 bullet.velocityX=speed;
 bullet.shapeColor = color(255);
 
@@ -19,23 +19,6 @@ function draw() {
   background(0);  
   drawSprites();
 
-if(wall.x-bullet.x < (bullet.width+wall.width)/2)
-{
-  bullet.velocityX=0;
-var deformation=0.5*weight*speed*speed/22500;
-if(deformation>180)
-{
-bullet.shapeColor =color(255,0,0) ;
-}
-if(deformation<180 && deformation>100)
-{
- bullet.shapeColor=color(230,230,0); 
-}
-if(deformation<100)
-{
- bullet.shapeColor=color(0,255,0) 
-}
-}
 }
 function hasCollided(lbullet,lwall)
 {bulletRightEdge=lbullet.x +lbullet.width;
@@ -63,30 +46,4 @@ if(damage<10)
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
